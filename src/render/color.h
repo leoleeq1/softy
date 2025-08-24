32cff709e552d6cb383150ef6e590fecd200ab3c
Kyuhan (Leo) Lee <leoleeidc@gmail.com>
2025-08-24 11:12:51 +0900

    chore: add new color constructor, default static color values

--- src/render/color.h
+++ src/render/color.h
@@ -5,6 +5,11 @@
 
 namespace softy {
 struct Color {
+  Color() : argb{} {}
+  Color(uint32_t argb) : argb{argb} {}
+  Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xFF)
+      : b{b}, g{g}, r{r}, a{a} {}
+
   union {
     uint32_t argb{};
     struct {
@@ -14,6 +19,12 @@ struct Color {
       uint8_t a;
     };
   };
+
+  static Color White() { return Color{0xFFFFFFFF}; };
+  static Color Black() { return Color{0xFF000000}; };
+  static Color Red() { return Color{0xFFFF0000}; };
+  static Color Green() { return Color{0xFF00FF00}; };
+  static Color Blue() { return Color{0xFF0000FF}; };
 };
 }  // namespace softy
 
