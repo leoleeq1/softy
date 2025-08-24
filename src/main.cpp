099b5145c1a1a3bbbde2567862456ccac7edcc30
Kyuhan (Leo) Lee <leoleeidc@gmail.com>
2025-08-24 11:15:41 +0900

    chore: update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <algorithm>
+#include <array>
 #include <chrono>
 #include <cstdint>
 #include <format>
 #include <print>
 #include <thread>
-#include <vector>
 
+#include "core/property.h"
+#include "core/transform.h"
 #include "math/math.h"
 #include "math/matrix.h"
 #include "math/vector.h"
+#include "render/buffer.h"
+#include "render/camera.h"
 #include "render/color.h"
+#include "render/forward_render_pipeline.h"
+#include "render/material.h"
+#include "render/mesh.h"
+#include "render/render_pipeline.h"
+#include "shader/shader.h"
 #include "window/window.h"
 
 using namespace std::chrono_literals;
@@ -22,62 +31,71 @@ int32_t main([[maybe_unused]] int32_t argc, [[maybe_unused]] char** argv) {
       .height = 600,
   };
   softy::Window window{};
-  std::vector<softy::Color> pixels(
-      static_cast<std::size_t>(descriptor.width * descriptor.height));
 
-  std::vector<softy::Color> test(
-      static_cast<std::size_t>(descriptor.width * descriptor.height / 2),
-      softy::Color{0xFF0000});
-  std::copy(test.begin(), test.end(), pixels.begin());
+  softy::ConstantBuffer cb{};
+  softy::ColorBuffer rt{descriptor.width, descriptor.height};
+  softy::DepthBuffer db{descriptor.width, descriptor.height};
+  std::unique_ptr<softy::RenderPipeline> renderPipeline(
+      new softy::ForwardRenderPipeline());
 
-  softy::v4f v0{0.0f, 1.0f, 2.0f, 3.0f};
-  softy::v4f v1{0.0f, 1.0f, 2.0f};
-  softy::v4f v2{v1};
-  softy::v4f v4 = softy::v4f::Zero();
-  softy::v4f v5 = softy::v4f::One();
-  softy::v4f v6 = softy::v4f::Basis(1);
-  softy::v2f v7{0.0f};
-  softy::v3f v8{0.0f, 1.0f};
-  softy::v2i v9{0};
-  softy::v3i v10{0, 1};
-  softy::v4i v11{0, 1, 2};
-  softy::mat4 m0{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-  softy::mat4 m1{m0};
-  softy::mat4 m2{softy::v2f{0, 1}, softy::v2f{2, 3}};
+  renderPipeline->SetConstantBuffer(&cb);
 
-  std::println("{}", softy::equals(v0, v0));
-  std::println("{}", v1);
-  std::println("{}", v2);
-  std::println("{}", v4);
-  std::println("{}", v5);
-  std::println("{}", v6);
-  std::println("{}", v7);
-  std::println("{}", v8);
-  std::println("{}", v9);
-  std::println("{}", v10);
-  std::println("{}\n", v11);
-  std::println("{}\n", m0);
-  std::println("{}\n", softy::Transpose(m0));
-  std::println("{}\n", m0 + m1);
-  std::println("{}\n", m0 - m1);
-  std::println("{}\n", m0 * m1);
-  std::println("{}\n", m2);
-
-  if (!window.Create(descriptor, channel, pixels)) {
+  if (!window.Create(descriptor, channel, rt.GetData())) {
     return EXIT_FAILURE;
   }
 
+  softy::Mesh cube{std::array{
+                       softy::Vertex(softy::v4f{-0.5f, -0.5f, -0.5f, 1.0f}),
+                       softy::Vertex(softy::v4f{-0.5f, -0.5f, +0.5f, 1.0f}),
+                       softy::Vertex(softy::v4f{+0.5f, -0.5f, +0.5f, 1.0f}),
+                       softy::Vertex(softy::v4f{+0.5f, -0.5f, -0.5f, 1.0f}),
+                       softy::Vertex(softy::v4f{-0.5f, +0.5f, -0.5f, 1.0f}),
+                       softy::Vertex(softy::v4f{-0.5f, +0.5f, +0.5f, 1.0f}),
+                       softy::Vertex(softy::v4f{+0.5f, +0.5f, +0.5f, 1.0f}),
+                       softy::Vertex(softy::v4f{+0.5f, +0.5f, -0.5f, 1.0f}),
+                   },
+                   std::array<std::size_t, 36>{
+                       0, 1, 2, 0, 2, 3, 0, 4, 7, 0, 7, 3, 1, 5, 4, 1, 4, 0,
+                       2, 6, 5, 2, 5, 1, 3, 7, 6, 3, 6, 2, 4, 5, 6, 4, 6, 7}};
+  softy::Transform cubeTransform{};
+
+  softy::Shader unlitColorShader{softy::UnlitColorShader()};
+  softy::Color color{0xFFFF0000};
+  softy::Material material{&unlitColorShader};
+  material.SetProperty("Color_", color);
+
+  softy::Camera cam{&rt};
+
+  auto start = std::chrono::high_resolution_clock::now();
   while (true) {
-    auto start = std::chrono::high_resolution_clock::now();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto dt = (end - start).count() / 1e9;
+    window.SetTitle(std::format("{} {:.2f}", descriptor.name, 1 / dt));
+
+    start = end;
 
     if (!window.Update()) {
       break;
     }
+
+    cam.GetTransform().position = softy::v3f{0.0f, 0.0f, -0.5f};
+    // cam.GetTransform().rotation += softy::v3f(0.0f, 60.0f * dt, 0.0f);
+    cam.GetTransform().LookAt(softy::v3f{0.0f, 0.0f, 0.0f}, true);
+    cubeTransform.scale = softy::v3f::One() * 2.0f;
+    cubeTransform.rotation += softy::v3f(0.0f, 60.0f * dt, 0.0f);
+
+    cb.SetData(softy::ConstantBufferData{
+        .matWorld = softy::mat4::Identity(),
+        .matView = cam.GetViewMatrix(),
+        .matProjection = cam.GetProjectionMatrix(),
+    });
+
+    renderPipeline->AddObject(&cube.GetVertexBuffer(), &cube.GetIndexBuffer(),
+                              cubeTransform.GetTRS(), &material);
+    renderPipeline->Render(&cam);
     window.Present();
 
-    auto end = std::chrono::high_resolution_clock::now();
-    auto diff = (end - start).count() / 1e9;
-    window.SetTitle(std::format("{} {:.2f}", descriptor.name, 1 / diff));
+    rt.Clear(softy::Color::Black());
   }
 
   return EXIT_SUCCESS;
