b2c873393ef328625f8c8352359c42744c5ba2a9
Kyuhan (Leo) Lee <leoleeidc@gmail.com>
2025-08-31 15:56:30 +0900

    feat: add Color to Vertex

--- src/render/vertex.h
+++ src/render/vertex.h
@@ -3,18 +3,21 @@
 
 #include "math/math.h"
 #include "math/vector.h"
+#include "render/color.h"
 
 namespace softy {
 struct Vertex {
   v4f position;
   v3f normal;
   v2f uv;
+  Color color;
 };
 
 struct VertexOutput {
   v4f position;
   v3f normal;
   v2f uv;
+  Color color;
 };
 
 constexpr Vertex lerp(Vertex v0, Vertex v1, float t) {
@@ -22,6 +25,7 @@ constexpr Vertex lerp(Vertex v0, Vertex v1, float t) {
   v.position = lerp(v0.position, v1.position, t);
   v.normal = lerp(v0.normal, v1.normal, t);
   v.uv = lerp(v0.uv, v1.uv, t);
+  v.color = lerp(v0.color, v1.color, t);
   return v;
 }
 
@@ -30,6 +34,17 @@ constexpr VertexOutput lerp(VertexOutput v0, VertexOutput v1, float t) {
   v.position = lerp(v0.position, v1.position, t);
   v.normal = lerp(v0.normal, v1.normal, t);
   v.uv = lerp(v0.uv, v1.uv, t);
+  v.color = lerp(v0.color, v1.color, t);
+  return v;
+}
+
+constexpr VertexOutput lerp(const VertexOutput& v0, const VertexOutput& v1,
+                            const VertexOutput& v2, v3f b) {
+  VertexOutput v{};
+  v.position = lerp(v0.position, v1.position, v2.position, b[0], b[1], b[2]);
+  v.normal = lerp(v0.normal, v1.normal, v2.normal, b[0], b[1], b[2]);
+  v.uv = lerp(v0.uv, v1.uv, v2.uv, b[0], b[1], b[2]);
+  v.color = lerp(v0.color, v1.color, v2.color, b[0], b[1], b[2]);
   return v;
 }
 }  // namespace softy
