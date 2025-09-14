aeba3312120bad7dd9be92bbb6d94d2f7bee4aea
Kyuhan (Leo) Lee <leoleeidc@gmail.com>
2025-09-15 00:22:46 +0900

    feat: add KeyDown, KeyUp events

--- src/event/window_event.h
+++ src/event/window_event.h
@@ -1,8 +1,16 @@
 #ifndef EVENT_WINDOW_EVENT_H_
 #define EVENT_WINDOW_EVENT_H_
 
+#include "input/keycode.h"
+
 namespace softy {
 struct WindowCreatedEvent {};
+struct WindowKeyDownEvent {
+  KeyCode key;
+};
+struct WindowKeyUpEvent {
+  KeyCode key;
+};
 }  // namespace softy
 
 #endif  // EVENT_WINDOW_EVENT_H_
